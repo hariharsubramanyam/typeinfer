e435a6a17c110e5808d448c9458e20ade9a1be27
Harihar Subramanyam <hahasubbu@gmail.com>
2015-04-18 11:50:10 -0400

    Test for messy numbers

--- test/test_typeinfer.py
+++ test/test_typeinfer.py
@@ -5,8 +5,8 @@ Ensure we can infer the type when everything is a number.
 '''
 def test_all_numbers():
     t = TypeInfer()
-    for i in range(1000):
-        t.add_value(i)
+    for i in xrange(1000):
+        t.add_value("%d" % (i,))
     assert t.infer_type() == "number"
 
 '''
@@ -14,7 +14,7 @@ Ensure we can infer the type when everything is a string.
 '''
 def test_all_strings():
     t = TypeInfer()
-    for i in range(1000):
+    for i in xrange(1000):
         t.add_value("something")
     assert t.infer_type() == "string"
 
@@ -23,8 +23,20 @@ Ensure we can infer the type when most of the values are numbers.
 '''
 def test_mostly_numbers():
     t = TypeInfer()
-    for i in range(1000):
-        t.add_value(i)
-    for i in range(10):
+    for i in xrange(1000):
+        t.add_value("%d" % (i,) )
+    for i in xrange(10):
         t.add_value("None")
     assert t.infer_type() == "number"
+
+'''
+Ensure that we can infer a numeric type even when the numbers are messy
+'''
+def test_messy_numbers():
+    t = TypeInfer()
+    for i in range(100):
+        t.add_value(" 102,000 ")
+    for i in range(5):
+        t.add_value("String")
+    assert t.infer_type() == "number"
+
